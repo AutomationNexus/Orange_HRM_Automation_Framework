29e46f657272a4ad02a44cfa79b1606d4e5b7881
angrybird04 <ujjwalsingh0407@gmail.com>
2024-05-18 22:30:43 +0530

    This is 1st commit of 13th May 2024 with Framework page.

--- pageObjects/Homepage.py
+++ pageObjects/Homepage.py
@@ -1,16 +1,19 @@
 from selenium.webdriver.common.by import By
 
-class Homepage:
 
-    frame = ("packageListFrame")
-    link = (By.LINK_TEXT, "org.openqa.selenium")
+class Homepage:
+    frame = (By.XPATH, '//*[@id="divInAppMarketing"]/iframe')
+    link = (By.XPATH, '//*[@id="in-app-content"]')
+
     def __init__(self, driver):
         self.driver = driver
 
     def framee(self):
-        self.driver.find_element(*Homepage.frame)
-
-    driver.switch_to.frame(frame)
-    driver.find_element(By.LINK_TEXT, "org.openqa.selenium").click()
+        self.driver.switch_to.frame(*Homepage.frame)
+        return self.driver.find_elements(*Homepage.link)
 
 
+
+
+#  driver.switch_to.frame(frame)
+#  driver.find_element(By.LINK_TEXT, "org.openqa.selenium").click()
